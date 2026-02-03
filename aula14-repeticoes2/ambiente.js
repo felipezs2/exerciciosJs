206a59a0a959881df196c22e9715c2a1d69687e4
Felipe <felipe.rodriantunes@gmail.com>
2026-02-03 15:10:16 -0300

    Depuração e estrutura for

---
+++ aula14-repeticoes2/ambiente.js
@@ -0,0 +1,8 @@
+// Depuração e estrutura for
+
+// 1 - Inicialização 2 - Teste lógico 3 - Encremento
+console.log ('Inicio')
+for (var contador = 1; contador <= 10; contador++) {
+    console.log (`${contador}º Passo`)
+}
+console.log ('Fim')
