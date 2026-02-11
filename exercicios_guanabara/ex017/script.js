b75712690c96ae1be10ad473eab47012f260a819
Felipe <felipe.rodriantunes@gmail.com>
2026-02-11 12:57:28 -0300

    Primeira resolução do exercicio de tabada utilizando estrutura de repetição

--- exercicios_guanabara/ex017/script.js
+++ exercicios_guanabara/ex017/script.js
@@ -2,7 +2,13 @@ function enviar() {
     var pegaNumero = document.querySelector('input#numero')
     var numero = Number(pegaNumero.value)
 
+    var retorno = document.querySelector('p#retorno')
+
     if (pegaNumero.value.length == 0) {
         window.alert ('Por favor digite um número')
+    } else {
+        for (var c = 0; c<=10 ; c++) {
+            retorno.innerHTML += `${c} * ${numero} = ${c*numero} <br>`
+        }
     }
 }
