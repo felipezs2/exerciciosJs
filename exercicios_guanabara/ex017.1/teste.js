724b0d8164671233e876d787680e99c54faee95c
Felipe <felipe.rodriantunes@gmail.com>
2026-02-16 10:57:28 -0300

    Utilizando o elemento select no exercicio de tabuada

---
+++ exercicios_guanabara/ex017.1/teste.js
@@ -0,0 +1,5 @@
+var numero = 5
+
+for (var c = 0; c<=10; c++){
+    console.log `${c*numero}`
+}
--- exercicios_guanabara/ex017/script.js
+++ exercicios_guanabara/ex017/script.js
@@ -1,14 +1,20 @@
 function enviar() {
     var pegaNumero = document.querySelector('input#numero')
     var numero = Number(pegaNumero.value)
+    var pegaTabela = document.querySelector('select#tabela')
 
     var retorno = document.querySelector('p#retorno')
 
     if (pegaNumero.value.length == 0) {
         window.alert ('Por favor digite um número')
+        retorno.innerHTML ('Por favor digite um número')
+        pegaTabela.innerHTML = ''
     } else {
+        pegaTabela.innerHTML = ''
         for (var c = 0; c<=10 ; c++) {
-            retorno.innerHTML += `${c} * ${numero} = ${c*numero} <br>`
+            var item = document.createElement('option')
+            item.text = `${numero} * ${c} = ${numero * c}`
+            pegaTabela.appendChild(item)
         }
     }
 }
