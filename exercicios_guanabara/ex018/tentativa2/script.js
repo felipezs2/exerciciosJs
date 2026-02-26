4e086889534bfd012184a2809f5e03d59f208abb
Felipe <felipe.rodriantunes@gmail.com>
2026-02-26 16:17:26 -0300

    ajuste do atributo defer

--- exercicios_guanabara/ex018/tentativa2/script.js
+++ exercicios_guanabara/ex018/tentativa2/script.js
@@ -1,23 +1,20 @@
 var getNumber = document.querySelector('input#number')
-var number = Number(getNumber.value)
 var returnValue = document.querySelector('p#return')
 
+/*
 function isNumber() {
-    if (number >= 1 && number <= 100) {
-        return true
-    }
-}
-
-function isNull() {
-    if (getNumber.length == 0) {
+    if (Number(getNumber.value) >= 1 || Number(getNumber.value) <= 100) {
         return true
     } else {
         return false
     }
 }
+*/
 
 function sendNumber () {
-    if (isNull) {
-        returnValue.innerHTML = `Num foi`
+    const value = Number(getNumber.value)
+
+    if (value != 1) {
+        returnValue.innerHTML = `AAAAAAAa`
     }
 }
