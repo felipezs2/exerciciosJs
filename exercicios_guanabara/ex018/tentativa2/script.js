d235add71acda0644d586d23ef2775c0d83b88de
Felipe <felipe.rodriantunes@gmail.com>
2026-02-28 13:50:50 -0300

    Finalizando a primeira etapa do exercicio 18 (Analisador de números

--- exercicios_guanabara/ex018/tentativa2/script.js
+++ exercicios_guanabara/ex018/tentativa2/script.js
@@ -1,20 +1,21 @@
 var getNumber = document.querySelector('input#number')
 var returnValue = document.querySelector('p#return')
+var list = []
 
-/*
-function isNumber() {
-    if (Number(getNumber.value) >= 1 || Number(getNumber.value) <= 100) {
-        return true
-    } else {
-        return false
-    }
-}
-*/
+function isNumber(n) {
+    return (Number(n) >= 1 && Number(n) <= 100) 
+}  // Se o numero for maior ou igual a 1 e menor ou igual a 100 o resultado é FALSO
+
+function inList(n, l) {
+    return l.indexOf(Number(n)) != -1
+} // Se o numero já estiver na lista retorne verdadeiro
 
 function sendNumber () {
-    const value = Number(getNumber.value)
-
-    if (value != 1) {
-        returnValue.innerHTML = `AAAAAAAa`
+    if (isNumber(getNumber.value) && !inList(getNumber.value, list)) {
+        returnValue.innerHTML = `Sucesso!`
+    } else if (!isNumber(getNumber.value)) {
+        returnValue.innerHTML = `Insira um número entre 1 e 100`
+    } else if (inList(getNumber.value), list) {
+        returnValue.innerHTML = `Número já adicionado na lista`
     }
 }
