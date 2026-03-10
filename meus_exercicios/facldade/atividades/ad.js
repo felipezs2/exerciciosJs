26906986f099eb6fefd76e95783912fc5b0ce99b
Felipe <felipe.rodriantunes@gmail.com>
2026-03-10 20:49:10 -0300

    Iniciando atividade diagnostica

---
+++ meus_exercicios/facldade/atividades/ad.js
@@ -0,0 +1,17 @@
+var lista =  [1, 5, 2, 3, 1, 3, 1]
+var moda = 0
+
+console.log (`${lista.length} | ${lista[0]}`)
+
+function calculaModa () {
+    for (contador = 0; contador < lista.length ; contador ++) {
+        let teste = lista[contador]
+        
+
+    if (teste == lista[contador]) {
+        console.log (`teste`)
+    }
+
+    }
+
+} calculaModa(lista)
